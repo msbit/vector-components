dbb4d147d312c39e12a4601641dfc07c4d758187
Tom Sullivan <tom@msbit.com.au>
2020-04-10 18:59:34 +1000

    Display perpendicular vectors for both A and B

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import { wrappedContext } from './context.js';
 import { mouseDrag, nearest } from './util.js';
+import { Vector } from './vector.js';
 
 window.addEventListener('load', async (event) => {
   const document = event.target;
@@ -8,13 +9,15 @@ window.addEventListener('load', async (event) => {
   const canvas = document.getElementById('canvas');
   const inputs = document.getElementsByTagName('input');
 
-  const context = wrappedContext(canvas.getContext('2d'), { x: -15, y: -10 }, { x: 15, y: 10 });
+  const context = wrappedContext(canvas.getContext('2d'), { x: -10, y: -10 }, { x: 10, y: 10 });
 
   let vectorIndex = -1;
   const draftVectors = [null, null];
   const draftVectorColours = ['lightblue', 'lightgreen'];
+  const draftPerpendicularVectors = [null, null];
   const vectors = [null, null];
   const vectorColours = ['blue', 'green'];
+  const perpendicularVectors = [null, null];
 
   function findChecked () {
     vectorIndex = [].findIndex.call(inputs, x => x.checked);
@@ -26,23 +29,27 @@ window.addEventListener('load', async (event) => {
 
   findChecked();
 
-  mouseDrag(canvas, event => nearest({ x: -15, y: -10 }, { x: 15, y: 10 }, event)).subscribe(({ start, end }) => {
+  mouseDrag(canvas, event => nearest({ x: -10, y: -10 }, { x: 10, y: 10 }, event)).subscribe(({ start, end }) => {
     if (vectorIndex === -1) { return; }
 
     if (end.x === 0 && end.y === 0) {
       draftVectors[vectorIndex] = null;
+      draftPerpendicularVectors[vectorIndex] = null;
       return;
     }
 
     draftVectors[vectorIndex] = end;
+    draftPerpendicularVectors[vectorIndex] = Vector.perpendicular(end);
   }, null, ({ start, end }) => {
     if (vectorIndex === -1) { return; }
 
     if (end.x !== 0 || end.y !== 0) {
       vectors[vectorIndex] = end;
+      perpendicularVectors[vectorIndex] = Vector.perpendicular(end);
     }
 
     draftVectors[vectorIndex] = null;
+    draftPerpendicularVectors[vectorIndex] = null;
   });
 
   window.setInterval(async () => {
@@ -54,8 +61,16 @@ window.addEventListener('load', async (event) => {
         context.drawVector(draftVectors[i], draftVectorColours[i]);
       }
 
+      if (draftPerpendicularVectors[i] !== null) {
+        context.drawVector(draftPerpendicularVectors[i], 'grey');
+      }
+
       if (vectors[i] !== null) {
         context.drawVector(vectors[i], vectorColours[i]);
+      }
+
+      if (perpendicularVectors[i] !== null) {
+        context.drawVector(perpendicularVectors[i], 'black');
       }
     }
   }, 40);
--- vector.js
+++ vector.js
@@ -1,20 +1,30 @@
-function magnitude ({ x, y }) {
-  return Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+class Vector {
+  constructor (x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  static magnitude ({ x, y }) {
+    return Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+  }
+
+  static dotProduct ({ x: aX, y: aY }, { x: bX, y: bY }) {
+    return aX * bX + aY + bY;
+  }
+
+  static normalise ({ x, y }) {
+    const magnitude = Vector.magnitude({ x, y });
+    x /= magnitude;
+    y /= magnitude;
+    return new Vector(x, y);
+  }
+
+  static perpendicular ({ x, y }) {
+    return Vector.normalise({
+      x: y,
+      y: -x
+    });
+  }
 }
 
-function normalise ({ x, y }) {
-  const mag = magnitude({ x, y });
-  return {
-    x: x / mag,
-    y: y / mag
-  };
-}
-
-function perp ({ x, y }) {
-  return normalise({
-    x: y,
-    y: -x
-  });
-}
-
-export { perp };
+export { Vector };
