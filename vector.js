8628dbd33aef341e4fe0443b8f4ab422ebe9a740
Tom Sullivan <tom@msbit.com.au>
2020-04-10 19:34:36 +1000

    Fix up vector issues

--- vector.js
+++ vector.js
@@ -4,12 +4,16 @@ class Vector {
     this.y = y;
   }
 
-  static magnitude ({ x, y }) {
-    return Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+  static add ({ x: aX, y: aY }, { x: bX, y: bY }) {
+    return new Vector(aX + bX, aY + bY);
   }
 
   static dotProduct ({ x: aX, y: aY }, { x: bX, y: bY }) {
-    return aX * bX + aY + bY;
+    return aX * bX + aY * bY;
+  }
+
+  static magnitude ({ x, y }) {
+    return Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
   }
 
   static normalise ({ x, y }) {
@@ -24,6 +28,10 @@ class Vector {
       x: y,
       y: -x
     });
+  }
+
+  static scalarProduct ({ x, y }, r) {
+    return new Vector(x * r, y * r);
   }
 }
 
