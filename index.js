4eef7ad447b6fd8fec8fc74fc2b7557bfdc8bec7
Tom Sullivan <tom@msbit.com.au>
2020-04-10 18:34:05 +1000

    Allow specifying which vector to set

--- index.js
+++ index.js
@@ -1,47 +1,62 @@
 import { wrappedContext } from './context.js';
 import { mouseDrag, nearest } from './util.js';
-import { perp } from './vector.js';
 
 window.addEventListener('load', async (event) => {
   const document = event.target;
   const window = event.currentTarget;
 
   const canvas = document.getElementById('canvas');
+  const inputs = document.getElementsByTagName('input');
 
   const context = wrappedContext(canvas.getContext('2d'), { x: -15, y: -10 }, { x: 15, y: 10 });
 
-  let draftVector;
-  let perpDraftVector;
-  const vectors = [];
+  let vectorIndex = -1;
+  const draftVectors = [null, null];
+  const draftVectorColours = ['lightblue', 'lightgreen'];
+  const vectors = [null, null];
+  const vectorColours = ['blue', 'green'];
+
+  function findChecked () {
+    vectorIndex = [].findIndex.call(inputs, x => x.checked);
+  }
+
+  [].forEach.call(inputs, (input) => {
+    input.addEventListener('change', findChecked);
+  });
+
+  findChecked();
 
   mouseDrag(canvas, event => nearest({ x: -15, y: -10 }, { x: 15, y: 10 }, event)).subscribe(({ start, end }) => {
+    if (vectorIndex === -1) { return; }
+
     if (end.x === 0 && end.y === 0) {
-      draftVector = null;
+      draftVectors[vectorIndex] = null;
       return;
     }
 
-    draftVector = end;
-    perpDraftVector = perp(draftVector);
+    draftVectors[vectorIndex] = end;
   }, null, ({ start, end }) => {
-    if (end.x !== 0 && end.y !== 0) {
-      vectors.push(end);
+    if (vectorIndex === -1) { return; }
+
+    if (end.x !== 0 || end.y !== 0) {
+      vectors[vectorIndex] = end;
     }
 
-    draftVector = null;
-    perpDraftVector = null;
+    draftVectors[vectorIndex] = null;
   });
 
-  window.setInterval(async (foo, bar, baz) => {
+  window.setInterval(async () => {
     context.clear();
     context.drawGrid();
 
-    vectors.forEach(v => context.drawVector(v, 'green'));
+    for (let i = 0; i < 2; i++) {
+      if (draftVectors[i] !== null) {
+        context.drawVector(draftVectors[i], draftVectorColours[i]);
+      }
 
-    if (draftVector) {
-      context.drawVector(draftVector, 'blue');
-    }
-    if (perpDraftVector) {
-      context.drawVector(perpDraftVector, 'red');
+      if (vectors[i] !== null) {
+        context.drawVector(vectors[i], vectorColours[i]);
+      }
     }
   }, 40);
 });
